eb9f5e1864929031c90e008b528483daa8f77443
suyons <su02ga@outlook.com>
2024-02-16 00:54:51 +0900

    Style: 태그 컴포넌트의 스타일 수정
    2024-02-16 00:54
    xl:block 추가

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -8,7 +8,7 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="text-sm font-medium uppercase leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+      className="text-sm font-medium uppercase leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 xl:block"
     >
       {text.split(' ').join('-')}
     </Link>
